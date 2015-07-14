791ba576f09702383f9585939efcc32f7ee4e135
= <=>
2015-07-14 14:39:21 -0400

    Added new debug functionality to easily retrieve request ids

--- src/Debug.php
+++ src/Debug.php
@@ -5,10 +5,22 @@ class Debug
 
     protected $request_id;
 
+    /**
+     * Set the request id
+     *
+     * @param $request_id
+     *
+     * @return $this
+     */
     public function setRequestId($request_id) {
         $this->request_id = $request_id;
+
+        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getRequestId() {
         return $this->request_id;
     }
